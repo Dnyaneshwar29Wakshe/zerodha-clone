e84d6e7d3bd3e4975b89dcfdaebba1c922f01e44
Dnyaneshwar29Wakshe <dnyaneshwarwakshe888@gmail.com>
2025-09-16 15:43:38 +0530

    Update HomePage.js

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -3,7 +3,7 @@ import Hero from "./Hero";
 import Awards from "./Awards";
 import Stats from "./Stats";
 import Pricing from "./Pricing";
-import Education from ".Education";
+import Education from "./Education";
 
 import OpenAccount from "../OpenAccount";
 import Navbar from "../Navbar";
